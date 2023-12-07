978d3a1432ae086b8191693a59c06d45daf59f92
andrianisaf <nisa.anisafitri@gmail.com>
2023-12-07 20:12:18 +0700

    index

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
+app.post('/submit-form', (req, res) => {
+    const formData = req.body;
+
+    console.log("Pesan Berhasil Dikirim:", formData);
+
+    res.json({ message: "Pesan Berhasil Dikirim"});
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
