65a1c4b5fbaf1bad9fcb93469f4df4abef868563
andrianisaf <nisa.anisafitri@gmail.com>
2023-12-09 09:48:05 +0700

    contact us

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const port = 3000;
+app.use(cors());
 
 app.post('/submit-form', (req, res) => {
     const formData = req.body;
 
     console.log("Pesan Berhasil Dikirim:", formData);
 
-    res.json('Pesan Berhasil Dikirim');
+    res.json({ message: 'Pesan Berhasil Dikirim'});
 });
 
 app.use(express.static('public'));
+
+const port = 3000;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server berjalan di http://localhost:${port}`);
